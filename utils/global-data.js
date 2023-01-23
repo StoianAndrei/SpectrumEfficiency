cbd2e9890208ab83eef7a88de6f0570b8dc13f68
StoianAndrei <47652458+StoianAndrei@users.noreply.github.com>
2023-01-24 10:41:01 +1300

    make a booking
    Let's see if I brake it

--- utils/global-data.js
+++ utils/global-data.js
@@ -5,9 +5,10 @@ export const getGlobalData = () => {
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Efficiency unleashed through meaningful intelligence';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+ /* const footerText = process.env.BLOG_FOOTER_TEXT
+ /*   ? decodeURI(process.env.BLOG_FOOTER_TEXT)
+ /*   : document.write('<a href="'+https://stoianandreimircea.zohobookings.com/#/customer/111+'">'+'Make a booking'+'</a>');*/
+const footerText = <a href="https://stoianandreimircea.zohobookings.com/#/customer/111">Make a booking</a>
 
   return {
     name,
